0a22e9ef4aae555a094d2a7c8d5e1f870a6515bc
Rachel Saraková <rachel.sarakova@student.tuke.sk>
2020-04-05 01:05:33 +0200

    second commit

--- article/articlesJS.js
+++ article/articlesJS.js
@@ -34,12 +34,10 @@ comFrmElm.addEventListener("submit", processOpnFrmData);
 
 function clearData(){
 
-    const oneDay = 1000*60*60*24;
-    //const date = (Date.now() - new Date(opinions[0].created))/oneDay;
-    const date = Date.now();
+    let oneDay = 1000*60*60*24;
 
     for(let i=0; i<opinions.length; i++){
-        if(Date.now()-new Date(opinions[i].created) > date){
+        if(Date.now()-new Date(opinions[i].created) > oneDay){
             opinions.splice(i,1);
         }
     }
@@ -63,7 +61,7 @@ function processOpnFrmData(event) {
     const theme3 = document.getElementById("interestedIn3").checked;
     const rating = document.getElementById("kvetyForm").elements["hodnotenie"];
 
-    if (name == "" || opn == "" || email == "") {
+    if (name === "" || opn === "" || email === "") {
         window.alert("Please, enter your name, email and opinion");
         return;
     }
@@ -79,7 +77,7 @@ function processOpnFrmData(event) {
             the1: theme1?"Zaujima tema kvetov":"",
             the2: theme2?"Zaujima tema pestovania":"",
             the3: theme3?"Zaujima tema kytic a ich pestovania":"",
-            rating: rating.value==1?"Hodnotenie:vyborna":"hodnotenie:priemerna",
+            rating: rating.value===1?"Hodnotenie:vyborna":"hodnotenie:priemerna",
             created: new Date()
         };
 
